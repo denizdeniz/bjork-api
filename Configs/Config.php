b23507176f62c8df9ef9213c3fac403938418855
denizvatan <deniz@jotform.com>
2018-01-14 21:11:20 +0300

    Congif:: base config added

--- Configs/Config.php
+++ Configs/Config.php
@@ -1,2 +1,13 @@
 <?
+namespace Configs;
+Class Config{
 
+	const DB_HOST = 'localhost';
+	const DB_USER = 'root';
+	const DB_PASSWORD = '';
+	const DB_NAME  = '';
+	const DB_PORT  = '';
+	const DB_CHARSET = 'utf8';
+	const BASE_URL = '';
+}
+?>
